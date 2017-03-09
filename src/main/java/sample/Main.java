aa3acd5fe1bb73289f3accc07aeeb623940b352e
MatthieuGenovese <gm102445@etu.unice.fr>
2017-03-10 00:40:05 +0100

    Debug du maven, (il fonctionne correctement maintenant)

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,23 +1,39 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Group root = new Group();
+        Scene scene = new Scene(root, 300, 250, Color.LIGHTGREEN);
+        Button btn = new Button();
+        btn.setLayoutX(100);
+        btn.setLayoutY(80);
+        btn.setText("Hello World");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World");
+            }
+        });
+        root.getChildren().add(btn);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
-        launch(args);
+
+        Application.launch(Main.class, args);
     }
 }
