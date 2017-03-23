e0adefec4be821e8cd8817058f5ef08d7cb672f3
Matthieu Genovese <matthieu.genovese@gmail.com>
2017-03-23 16:29:58 +0100

    Mise en place plus propre des différents éléments pour la selection du CSV. Ajout d'un bouton valider qui permet de valider l'ouverture du fichier selectionné (il ne s'affiche pas tant qu'on a pas sélectionné de fichier)

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,5 +1,6 @@
 package sample;
 
+import com.sun.javafx.scene.text.TextLine;
 import cplex.Carseq;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
@@ -35,22 +36,39 @@ public class Main extends Application {
     final TabPane gestionnaireDonglet = new TabPane();
     //le bouton ... qui ouvre l'explorer pour chercher les fichiers csv
     final Button btnImport= new Button("...");
+    final Button btnValiderImport = new Button("Valider");
 
     final Text textImport = new Text();
+
+    final TextField textFilePath = new TextField();
 
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Speed Dating");
         textImport.setText("Fichier à importer :");
+        textImport.setLayoutX(scene.getWidth() * 20 / 100);
+        textImport.setLayoutY(scene.getHeight() * 10 / 100);
+        textFilePath.setMaxWidth(scene.getWidth() * 40 / 100);
+        textFilePath.setEditable(false);
+        textFilePath.setPrefWidth(200);
+        textFilePath.setPrefHeight(20);
+        textFilePath.setLayoutX(textImport.getLayoutX() + 100);
+        textFilePath.setLayoutY(textImport.getLayoutY() - textFilePath.getPrefHeight());
+        btnImport.setLayoutX(textFilePath.getLayoutX() + textFilePath.getPrefWidth() + 10);
+        btnImport.setLayoutY(textFilePath.getLayoutY());
+        btnValiderImport.setLayoutX(textFilePath.getLayoutX() + textFilePath.getPrefWidth() + 10);
+        btnValiderImport.setLayoutY(textFilePath.getLayoutY() + textFilePath.getPrefHeight()*2);
+        btnValiderImport.setVisible(false);
+
         //on ajoute au bouton "..." une action : Un file chooser (cela ouvre l'explorer), puis on lui applique un filtre pour qu'il n'affiche que les csv
 
         //ajout de l'onglet "import" au gestionnaire d'onglet
         gestionnaireDonglet.getTabs().setAll(ongletImport);
         //Ajout du bouton "..." et du textImport au groupe import
-        groupImport.getChildren().add(btnImport);
         groupImport.getChildren().add(textImport);
-        textImport.setLayoutX(scene.getWidth() / 2);
-        textImport.setLayoutY(scene.getHeight() / 2);
+        groupImport.getChildren().add(textFilePath);
+        groupImport.getChildren().add(btnValiderImport);
+        groupImport.getChildren().add(btnImport);
         //assignation du groupe "groupImport" a l'onglet Import
         ongletImport.setContent(groupImport);
         ongletImport.setClosable(false);
@@ -70,30 +88,38 @@ public class Main extends Application {
             dialog.getExtensionFilters().setAll(new FileChooser.ExtensionFilter("Fichiers csv", "*.csv"));
             final File file = dialog.showOpenDialog(btnImport.getScene().getWindow());
             if (file != null) {
-                try {
-                    csvManager = new CSVManager(file.getAbsolutePath());
-                    ArrayList<Personne> listeChargee = new ArrayList<>();
-                    listeChargee = csvManager.getPersonnesFromCSV();
-                    for(Personne p : listeChargee){
-                        System.out.println(p);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
+                textFilePath.setText(file.getAbsolutePath());
+                btnValiderImport.setVisible(true);
+            }
+        });
+        btnValiderImport.setOnAction(actionEvent -> {
+            try {
+                csvManager = new CSVManager(textFilePath.getText());
+                ArrayList<Personne> listeChargee = new ArrayList<>();
+                listeChargee = csvManager.getPersonnesFromCSV();
+                for(Personne p : listeChargee){
+                    System.out.println(p);
                 }
-
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
         //ajout d'un listener sur le resize de la scene, ecoute le resize du width, mets a jour la position du texte en fonction
         scene.widthProperty().addListener(new ChangeListener<Number>() {
             @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneWidth, Number newSceneWidth) {
-                textImport.setLayoutX((Double) newSceneWidth / 2);
-                System.out.println((Double) newSceneWidth / 2);
+                textImport.setLayoutX((Double) newSceneWidth * 20 / 100);
+                textFilePath.setLayoutX(textImport.getLayoutX() + 100);
+                btnImport.setLayoutX(textFilePath.getLayoutX() + textFilePath.getPrefWidth() + 10);
+                btnValiderImport.setLayoutX(textFilePath.getLayoutX() + textFilePath.getPrefWidth() + 10);
             }
         });
         //ajout d'un listener sur le resize de la scene, ecoute le resize du height, mets a jour la position du texte en fonction
         scene.heightProperty().addListener(new ChangeListener<Number>() {
             @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneHeight, Number newSceneHeight) {
-                textImport.setLayoutY((Double) newSceneHeight / 2);
+                textImport.setLayoutY((Double) newSceneHeight * 10 / 100);
+                textFilePath.setLayoutY(textImport.getLayoutY() - textFilePath.getPrefHeight());
+                btnImport.setLayoutY(textFilePath.getLayoutY());
+                btnValiderImport.setLayoutY(textFilePath.getLayoutY() + textFilePath.getPrefHeight()*2);
             }
         });
     }
